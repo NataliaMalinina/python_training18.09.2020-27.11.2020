e5c07972ed31ca581bd77803d4b27e64221902aa
Natalia Malinina <proxorova1992@gmail.com>
2020-09-26 20:39:28 +0700

    созданы пакеты для разных зон ответственности

--- test/add_user.py
+++ test/add_user.py
@@ -11,11 +11,11 @@ def app(request):
 
 
 def test_add_user(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.fill_in_form(Parameters(firstname="Iona", middlename="Tasha Fox", lastname="Donovan",
         company="Rhodes Steele Co", address="Numquam nisi enim ad", home="Laborum Ut delectus",
         mobile="Eum harum est asperi", work="Consequatur Rerum o", email="dadicef@mailinator.net",
         byear="2006", bday="26", bmonth="July", new_group="[none]"))
-    app.log_out_user()
+    app.session.log_out_user()
 
 
