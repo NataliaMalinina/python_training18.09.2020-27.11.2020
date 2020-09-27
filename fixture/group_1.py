8c24da0c55f64d89c27e2f6dd47a183944782dfc
Natalia Malinina <proxorova1992@gmail.com>
2020-09-27 10:31:03 +0700

    сделала общую фикстуру, добавлен тест delete group

--- fixture/group_1.py
+++ fixture/group_1.py
@@ -28,6 +28,13 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        wd.find_element_by_name("selected[]").click() # select first group
+        wd.find_element_by_name("delete").click()# submit delete group
+        self.return_groups_page()
+
     def return_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
--- test/add_user.py
+++ test/add_user.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
+
 from model.params_for_user import Parameters
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_user(app):
