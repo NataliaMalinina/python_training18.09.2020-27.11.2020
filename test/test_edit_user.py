b3c174e6b0343d51008740fa05c3108af66e85f5
Natalia Malinina <proxorova1992@gmail.com>
2020-09-29 21:09:27 +0700

    сделала параметры для редактирования юзера Задание № 7

--- test/test_edit_user.py
+++ test/test_edit_user.py
@@ -1,6 +1,9 @@
-
+from model.params_for_user import Parameters
 
 def test_edit_user(app):
     app.session.login(username="admin", password="secret")
-    app.user.edit_user()
+    app.user.edit_user(Parameters(firstname="Thane", middlename="Omar Kennedy", lastname="Snider",
+        company="Calhoun Carter Trading", address="Distinctio Amet no", home="Cillum veniam eveni",
+        mobile="Dolor sint perspicia", work="Voluptas voluptatibu", email="giwige@mailinator.net",
+        byear="2000", bday="5", bmonth="September", new_group="[none]"))
     app.session.logout()
