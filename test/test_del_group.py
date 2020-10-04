8156689ba6803727ecdc6dcda53c7cf520061179
Natalia Malinina <proxorova1992@gmail.com>
2020-10-04 10:37:45 +0700

    доработан метод удаления группы - если нет ни одной, создавать принудительно и удалять

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,3 +1,6 @@
+from model.group import Group
 
 def test_delete_first_group(app):
+    if app.group_1.count() == 0:
+        app.group_1.create(Group(name= "тест"))
     app.group_1.delete_first_group()
