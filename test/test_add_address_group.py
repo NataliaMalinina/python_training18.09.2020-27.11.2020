6ff36f56654ba90411fde149fed484e75a24e5cc
Natalia Malinina <proxorova1992@gmail.com>
2020-10-10 17:20:46 +0700

    проверки путём сравнения списков в тестах для создания, редактирования и удаления групп

--- test/test_add_address_group.py
+++ test/test_add_address_group.py
@@ -2,11 +2,24 @@
 from model.group import Group
 
 
+
 def test_address_group(app):
-    app.group_1.create(Group(name="Университет", header="Кафедра №1", footer="Кафедра №2"))
+    old_groups = app.group_1.get_group_list()
+    group = Group(name="Университет", header="Кафедра №1", footer="Кафедра №2")
+    app.group_1.create(group)
+    new_groups = app.group_1.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
-      app.group_1.create(Group(name=" ", header=" ", footer=" "))
+    old_groups = app.group_1.get_group_list()
+    group = Group(name="", header="", footer="")
+    app.group_1.create(group)
+    new_groups = app.group_1.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,9 +2,19 @@ from model.group import Group
 
 
 def test_edit_group(app):
+    old_groups = app.group_1.get_group_list()
+    group = Group(name="Грибоедова")
+    group.id = old_groups[0].id
     if app.group_1.count() == 0:
         app.group_1.create(Group(name="Школа", header="Номер 2", footer="Номер 3"))
-    app.group_1.edit_group(Group(name="Грибоедова"))
+    app.group_1.edit_group(group)
+    new_groups = app.group_1.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_edit_group(app):
-    app.group_1.edit_group(Group(footer="New footer"))
+# def test_edit_group(app):
+#     old_groups = app.group_1.get_group_list()
+#     app.group_1.edit_group(Group(footer="New footer"))
+#     new_groups = app.group_1.get_group_list()
+#     assert len(old_groups) == len(new_groups)
