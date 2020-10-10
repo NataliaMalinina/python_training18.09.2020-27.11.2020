0d725fbb46c9ea2d3372864bb069f8e774df12b4
Natalia Malinina <proxorova1992@gmail.com>
2020-10-10 19:08:45 +0700

    сравнение списков для удаления пользователя и редактирования

--- test/test_del_user.py
+++ test/test_del_user.py
@@ -4,4 +4,7 @@ from model.params_for_user import Parameters
 def test_del_user(app):
     if app.user.count() == 0:
         app.user.fill(Parameters(firstname= "тест", bday="0", bmonth="-"))
+    old_user = app.user.get_user_list()
     app.user.del_user()
+    new_user = app.user.get_user_list()
+    assert len(old_user) - 1 == len(new_user)
--- test/test_edit_user.py
+++ test/test_edit_user.py
@@ -2,10 +2,13 @@ from model.params_for_user import Parameters
 
 
 def test_edit_user(app):
+    old_user = app.user.get_user_list()
     if app.user.count() == 0:
         app.user.fill(Parameters(firstname= "тест", bday="0", bmonth="-"))
     app.user.edit_user((Parameters(firstname="Thane", middlename="Omar Kennedy", lastname="Snider",
         company="Calhoun Carter Trading", address="Distinctio Amet no", home="Cillum veniam eveni",
         mobile="Dolor sint perspicia", work="Voluptas voluptatibu", email="giwige@mailinator.net",
         byear="2000", bday="5", bmonth="September", new_group="[none]")))
+    new_user = app.user.get_user_list()
+    assert len(old_user) == len(new_user)
 
