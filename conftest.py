b2d8856278e9b6dc6d698f2907da4b1fd0874cb2
Natalia Malinina <proxorova1992@gmail.com>
2020-10-23 21:13:09 +0700

    Задание №17: Реализовать загрузку информации о тестируемом приложении из конфигурационного файла. создан файл target.json, внесены изменения в фикстуру

--- conftest.py
+++ conftest.py
@@ -1,23 +1,25 @@
 
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
+target = None
 
 @pytest.fixture()
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    login = request.config.getoption("--login")
-    password = request.config.getoption("--password")
-
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, login=login, password=password) #инициализация фикстуры
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url, login=login, password=password)  # инициализация фикстуры
-    fixture.session.ensure_login(username=login, password=password)
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target")) #путь к файлу
+        with open(config_file) as f: #переменная f содержит объект, который указывает на открытый файл
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid:
+        fixture = Application(browser=browser, base_url=target['baseUrl'], login=target['username'],
+                              password=target['password']) #создаём фикстуру если она None или не валидная
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -30,6 +32,6 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
     parser.addoption("--login", action="store")
     parser.addoption("--password", action="store")
