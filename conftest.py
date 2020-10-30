917d40237c25f7d3d84f0cbe7cc9bb4a0bcd0a36
Natalia Malinina <proxorova1992@gmail.com>
2020-10-30 22:22:36 +0700

    добавлена фикстура для БД и изменён файл target.json

--- conftest.py
+++ conftest.py
@@ -5,24 +5,43 @@ import json
 import os.path
 import importlib
 import jsonpickle
+from fixture.db import DbFixture
 
 fixture = None
 target = None
 
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file) #путь к файлу
+        with open(config_file) as f: #переменная f содержит объект, который указывает на открытый файл
+            target = json.load(f)
+    return target
+
+
 @pytest.fixture()
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target")) #путь к файлу
-        with open(config_file) as f: #переменная f содержит объект, который указывает на открытый файл
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))['web'] #берем из конфигурации блок web
     if fixture is None or not fixture.is_valid:
-        fixture = Application(browser=browser, base_url=target['baseUrl'], login=target['username'],
-                              password=target['password']) #создаём фикстуру если она None или не валидная
-    fixture.session.ensure_login(username=target['username'], password=target['password'])
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'], login=web_config['username'],
+                              password=web_config['password']) #создаём фикстуру если она None или не валидная
+    fixture.session.ensure_login(username=web_config['username'], password=target['password'])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db'] #берем из конфигурации блок db
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'],
+                            password=db_config['password'])
+    def end():
+        dbfixture.destroy()
+    request.addfinalizer(end)
+    return dbfixture
+
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
